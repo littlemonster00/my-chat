2d8d2c5294abf090e61c8f53efa55fdaa4bea055
littlemonster00 <sang.lequang94@gmail.com>
2020-03-06 15:27:07 +0700

    Config webpack with babel

---
+++ app/src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const jsx = <div>Hello webpack</div>;
+
+ReactDOM.render(jsx, document.getElementById("app"));
---
+++ app/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public/dist/"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{ loader: "babel-loader", test: /\.js$/, exclude: /node_modules/ }]
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  }
+};
