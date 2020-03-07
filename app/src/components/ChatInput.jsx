134dcb8b8f100384e685d708b01fdd47f91c54db
littlemonster00 <sang.lequang94@gmail.com>
2020-03-07 20:49:40 +0700

    Add chat input

--- app/src/components/ChatInput.jsx
+++ app/src/components/ChatInput.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "../styles/components/ChatInput.scss";
 export class ChatInput extends React.Component {
   render() {
-    return <div className="chat-input">This is chat inputs</div>;
+    return (
+      <div className="chat-input">
+        <textarea className="form-control" rows="3" id="comment"></textarea>
+        <button className="btn btn-success">Send</button>
+      </div>
+    );
   }
 }
