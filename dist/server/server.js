c1f26b01a112e77e32f358e9b6d65a0a8d641017
littlemonster00 <sang.lequang94@gmail.com>
2020-03-07 20:38:25 +0700

    Fix max width for windows

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,22 +1,12 @@
 "use strict";
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
-var _a = require("apollo-server"), ApolloServer = _a.ApolloServer, gql = _a.gql;
-// The GraphQL schema
-var typeDefs = gql(__makeTemplateObject(["\n  type Query {\n    \"A simple type for getting started!\"\n    hello: String\n  }\n"], ["\n  type Query {\n    \"A simple type for getting started!\"\n    hello: String\n  }\n"]));
-// A map of functions which return data for the schema.
-var resolvers = {
-    Query: {
-        hello: function () { return "world"; }
-    }
-};
-var server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
+var express = require("express");
+var server = express();
+var port = process.env.PORT || 3000;
+server.get("/", function (req, res) {
+    res.send({
+        message: "Welcome to server"
+    });
 });
-server.listen().then(function (_a) {
-    var url = _a.url;
-    console.log("🚀 Server ready at ", url);
+server.listen(port, function () {
+    console.log("server is running up ", port);
 });
