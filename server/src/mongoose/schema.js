39b45475eea4521d7e6c50c8eec637e569f213a1
littlemonster00 <sang.lequang94@gmail.com>
2020-03-10 14:38:40 +0700

    Add mutation login

--- server/src/mongoose/schema.js
+++ server/src/mongoose/schema.js
@@ -5,6 +5,14 @@ const userSchema = new Schema({
   username: String,
   password: String
 });
-
+const messageSchema = new Schema({
+  text: String,
+  author: String,
+  createdAt: String,
+  lastSeen: String
+});
+// Decleration models
 const User = mongoose.model("User", userSchema);
-module.exports = User;
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = { User, Message };
--- server/src/resolvers/index.js
+++ server/src/resolvers/index.js
@@ -1,11 +1,18 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../mongoose/schema");
 const resolvers = {
   Query: {
     announcement: () =>
       `Say hello to the new Apollo Server! A production ready GraphQL server with an incredible getting started experience.`
   },
   Mutation: {
-    login: (parent, { username, password }) => {
-      if ((username === "sangle", password === "123abc")) return "sangle123abc";
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username }, "password");
+      if (user.password === password) {
+        const token = jwt.sign({ id: user._id, username }, "123abc");
+        return token;
+      }
+      return null;
     }
   }
 };
