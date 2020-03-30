e7669aeb20acd410a8b94609e61d8376b69d9f20
littlemonster00 <sang.lequang94@gmail.com>
2020-03-30 10:39:11 +0700

    Server update config for mongodb

--- server/src/config/mongo-db.js
+++ server/src/config/mongo-db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-const url = "mongodb://localhost:27017/graphqldb";
-
+// const url = "mongodb://localhost:27017/graphqldb";
+const url = process.env.MONGO_URI;
 mongoose.connect(url, { useNewUrlParser: true });
 mongoose.connection.once("open", () =>
-  console.log(`Connected to mongo at ${url}`)
+  console.log(`Connected to mongo at: ${url.split("@")[2]}`)
 );
 
 /* const User = require("../mongoose/schema");
